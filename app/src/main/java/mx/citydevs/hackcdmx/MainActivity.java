2d70f196ff92c91d9feae90472aaf0c0fc15e0cb
mikesaurio <mikesaurio@gmail.com>
2015-09-16 20:30:10 -0500

    se elimina codigo basura del main

--- app/src/main/java/mx/citydevs/hackcdmx/MainActivity.java
+++ app/src/main/java/mx/citydevs/hackcdmx/MainActivity.java
@@ -18,49 +18,15 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //setSupportActionBar();
         initUI();
     }
 
-    protected void setSupportActionBar() {
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.actionbar);
-        mToolbar.setTitle("");
-        mToolbar.getBackground().setAlpha(255);
-        TextView actionbarTitle = (TextView) mToolbar.findViewById(R.id.actionbar_title);
-        actionbarTitle.setText("");
-        actionbarTitle.setTextColor(getResources().getColor(R.color.colorWhite));
-
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setElevation(5);
-    }
 
     private void initUI() {
         findViewById(R.id.main_btn_officer).setOnClickListener(this);
         findViewById(R.id.main_btn_infraction).setOnClickListener(this);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_aboutme) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    
 
     @Override
     public void onClick(View v) {
